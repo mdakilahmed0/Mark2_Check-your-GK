95999e814f2cea2c6abd2093c269eaa278a781cc
Md Akil Ahmed <48244460+mdakilahmed0@users.noreply.github.com>
2022-08-30 13:03:09 +0000

    Fixed spcaing

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var readlineSync =  require ("readline-sync");
+var readlineSync = require("readline-sync");
 var score = 0;
 
 var questions = [
   {
-  question: "Whos is the current elected president of India? ",
-  answer: "Droupadi Murmu"
+    question: "Whos is the current elected president of India? ",
+    answer: "Droupadi Murmu"
   },
   {
-  question: "Who is the finance minister of India? ",
-  answer: "Nirmala"
+    question: "Who is the finance minister of India? ",
+    answer: "Nirmala"
   },
   {
-  question: "Who is the founder of Boring company? ",
+    question: "Who is the founder of Boring company? ",
     answer: "Elon Musk"
   },
   {
@@ -24,11 +24,11 @@ var questions = [
   },
   {
     question: "How many consonants are there in the English alphabet? ",
-  answer: "21"
+    answer: "21"
   },
   {
     question: "Name the National river of India? ",
-      answer: "Ganga"
+    answer: "Ganga"
   },
   {
     question: "Which colour symbolises peace? ",
@@ -50,7 +50,7 @@ var highScorer = [
     score: 10
   },
   {
-    name:"Allen",
+    name: "Allen",
     score: 9
   },
   {
@@ -65,34 +65,35 @@ var highScorer = [
 
 function greeting() {
   var username = readlineSync.question("What is your name, Sir? ");
-  console.log("Welcome "+username+ " to the quiz! ")
+  console.log("\n");
+  console.log("Welcome " + username + " to the quiz! ")
+  console.log("\n");
 }
-
-function play(question, answer){
+function play(question, answer) {
   var userAnswer = readlineSync.question(question);
-  if(userAnswer.toUpperCase() == answer.toUpperCase()){
+  if (userAnswer.toUpperCase() == answer.toUpperCase()) {
     console.log("Correct!");
     score++;
   } else {
     console.log("Wrong answer!");
     score--;
   }
-  console.log("Your current score is "+ score);
+  console.log("Your current score is " + score);
   console.log("----------------------")
 }
 
-function playGame(){
-  for(var i=0; i<questions.length; i++){
+function playGame() {
+  for (var i = 0; i < questions.length; i++) {
     var currentQuestion = questions[i];
     play(currentQuestion.question, currentQuestion.answer);
   }
 }
 
-function scoreCheck(){
-  console.log("Well Done! Your score is "+ score + "/10");
+function scoreCheck() {
+  console.log("Well Done! Your score is " + score + "/10");
   console.log("Compare your score with high scorer, if your's is higher, ping me, I'll add your as high scorer, Thanks!")
   console.log("------------------------------------");
-  highScorer.map(score => console.log(score.name," : ", score.score))
+  highScorer.map(score => console.log(score.name, " : ", score.score))
 }
 
 greeting();
